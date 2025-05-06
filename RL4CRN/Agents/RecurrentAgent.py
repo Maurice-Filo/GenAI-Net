Fix numpy broadcasting bug in the dynamic error computation for the loss. Also rank implementation succesful.

RL4CRN/Agents/RecurrentAgent.py
@@ -1,6 +1,7 @@
 import torch
 import time
-from RL4CRN.Abstract.AbstractAgent import AbstractAgent
+import numpy as np
+from RL4CRN.Agents.AbstractAgent import AbstractAgent
 
 class RecurrentAgent(AbstractAgent):
     def __init__(self, env, completor, allow_input_influence=False, logger=None, learning_rate=1e-3, entropy_weight=0.01, entropy_update_coefficient=0.9, entropy_schedule=20, minimum_entropy_weight=1, risk=0.8, risk_update=0.00, max_risk=1.00, risk_schedule=20):
@@ -70,7 +71,6 @@ class RecurrentAgent(AbstractAgent):
         entropy_mean = torch.mean(self.last_entropy)
         n_samples = self.last_logP.shape[0]
         # Compute the loss that is used to compute the gradient
-        # loss_for_gradient =  (loss_for_each_sample.detach() * self.last_logP) - self.entropy_weight * entropy_mean - (self.entropy_weight * entropy_mean.detach() * self.last_logP)
         loss_for_gradient =  (loss_for_each_sample * self.last_logP) - self.entropy_weight * entropy_mean - (self.entropy_weight * entropy_mean.detach() * self.last_logP)
         # Risky policy gradient
         top_k = torch.topk(loss_for_each_sample, int(n_samples * (1.-self.risk)), largest=False).indices
@@ -97,7 +97,7 @@ class RecurrentAgent(AbstractAgent):
             self.logger.log_metric('backward_time', toc_backward - tic_backward)
             best = loss_for_each_sample[top_k[0]]
             worst = loss_for_each_sample[top_k[-1]]
-            avg = loss_for_each_sample[top_k].mean()
+            avg = loss_for_each_sample[top_k].float().mean()
             self.logger.log_metric('best_loss', best.item())
             self.logger.log_metric('worst_loss', worst.item())
             self.logger.log_metric('avg_loss', avg.item())

RL4CRN/Input_Output_Rxn_Networks/IOCRN_MassAction.py
@@ -22,8 +22,7 @@ class IOCRN_MassAction:
         self.output_species = output_species
         self.num_unknown_parameters = np.isnan(parameters).sum()
         self.nan_indices = np.where(np.isnan(parameters))[0]
-        self.last_trajectories = None
-        self.last_time_horizon = None
+        self.last_task_info = {}
 
     def clone(self):
         return copy.deepcopy(self)
@@ -45,8 +44,7 @@ class IOCRN_MassAction:
 
     def tune_reaction(self, reaction_index, value):
         # Flush the trajectory memory
-        self.last_trajectories = None
-        self.last_time_horizon = None
+        self.last_task_info = {}
 
         if reaction_index < 0 or reaction_index >= self.num_reactions:
             raise ValueError("Reaction index out of bounds.")
@@ -56,8 +54,7 @@ class IOCRN_MassAction:
 
     def set_parameters(self, index, value):
         # Flush the trajectory memory
-        self.last_trajectories = None
-        self.last_time_horizon = None
+        self.last_task_info = {}
         
         self.parameters[index] = value
         self.num_unknown_parameters = np.isnan(self.parameters).sum()
@@ -65,8 +62,7 @@ class IOCRN_MassAction:
 
     def add_reaction(self, reaction):
         # Flush the trajectory memory
-        self.last_trajectories = None
-        self.last_time_horizon = None
+        self.last_task_info = {}
         # reaction is a dictionary with keys 'reactants index', 'products index', 'input influence index', 'rate constant'
         reactant1_index, reactant2_index = self.map_index_to_species(reaction['reactants index'])
         product1_index, product2_index = self.map_index_to_species(reaction['products index'])
@@ -106,16 +102,16 @@ class IOCRN_MassAction:
         return np.matmul(self.stoichiometry_matrix, self.propensity_function(concentrations, inputs))
 
     def transient_response(self, inputs, initial_condition, time_horizon, return_states=False):
-        if not return_states and self.last_trajectories is not None:
-            return self.last_trajectories
+        if not return_states and self.last_task_info is not None:
+            return self.last_task_info['trajectories']
         
         outputs = []
         def stop_if_unstable(t, y):
             """Event function to stop integration if solution becomes unstable."""
             threshold = 10000  # Adjust as needed
             outputs = threshold - np.max(y)
-            self.last_trajectories = outputs
-            self.last_time_horizon = time_horizon
+            self.last_task_info['trajectories'] = outputs
+            self.last_task_info['time_horizon'] = time_horizon
             return outputs
         
         stop_if_unstable.terminal = True  # Stop integration if triggered
@@ -135,23 +131,23 @@ class IOCRN_MassAction:
                 output = np.pad(output, ((0, time_horizon.shape[0] - output.shape[0]), (0,0)), mode='constant', constant_values=1000.0)
             outputs.append(output)  
 
-        self.last_trajectories = outputs
-        self.last_time_horizon = time_horizon
+        self.last_task_info['trajectories'] = outputs
+        self.last_task_info['time_horizon'] = time_horizon
         if return_states:
             return outputs, solution
         
         return outputs
     
     def plot_transient_response(self, fig=None, axes=None):
-        if self.last_trajectories is None:
+        if self.last_task_info is None:
             raise ValueError("No transient response data available. Run transient_response() first.")
         if fig is None and axes is None:
             fig, axes = plt.subplots(self.num_outputs, 1, figsize=(10, 5 * self.num_outputs))
             if not isinstance(axes, (list, np.ndarray)):
                 axes = [axes]
         for i in range(self.num_outputs):
-            for j in range(len(self.last_trajectories)):
-                axes[i].plot(self.last_time_horizon, self.last_trajectories[j], alpha=0.1)
+            for j in range(len(self.last_task_info['trajectories'])):
+                axes[i].plot(self.last_task_info['time_horizon'], self.last_task_info['trajectories'][j], alpha=0.1)
                 axes[i].set_title(f"Transient Response of Output Species {self.output_species[i]}")
                 axes[i].set_xlabel("Time")
                 axes[i].set_ylabel("Concentration")

RL4CRN/Rewards/Transients.py
@@ -10,8 +10,9 @@ def performance_metric(r, y):
 
     # reshape weight to match the shape of y
     weight = np.repeat(weight, y.shape[1]*y.shape[2]).reshape(y.shape[0], y.shape[1], y.shape[2])
-
-    return (weight*np.abs(r - y)).mean()
+    r_reshaped = r.reshape(1, r.shape[0], 1)
+    r_tiled = np.tile(r_reshaped, (y.shape[0], 1, 1))
+    return (weight*np.abs(r_tiled - y)).mean()
 
 def ss_metric(r, y_ss):
     return np.abs(r - y_ss)
@@ -22,4 +23,5 @@ def dynamic_tracking_error(crn, inputs, initial_condition, time_horizon, r, thre
     y = np.maximum(y,0)
     y[np.isnan(y)] = threshold
     y[np.isinf(y)] = threshold
-    return performance_metric(r, y), y, time_horizon
+    perf = performance_metric(r, y)
+    return perf, {'task': 'transients', 'reward': perf, 'trajectories': y, 'time_horizon': time_horizon, 'setpoint': r}